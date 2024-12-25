ec43e62626430a1fa23f873aded2bc4089f223b2
theshreeraj <shreerajmane007@gmail.com>
2024-12-25 23:56:23 +0530

    [bugfix] :

--- config/mongodb.js
+++ config/mongodb.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
 
     try{
         mongoose.connection.on('connected', () => console.log("Database Connected"))
-        await mongoose.connect("mongodb+srv://shreerajmane007:1234567890@cluster0.bkgzb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        await mongoose.connect("mongodb+srv://decodersclub:shreeraj@cluster0.5i0uu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     }catch(error) {
         console.error(error)
     }
