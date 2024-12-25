893ecdcadde5d6fc6ec5d5f8f0aeaa7cdabd70d5
theshreeraj <shreerajmane007@gmail.com>
2024-12-25 20:47:17 +0530

    [bugfix]

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,16 +1,7 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 mongoose
-  .connect(
-    process.env.MONGO_URI ||
-      "mongodb+srv://shreerajmane007:1234567890@cluster0.bkgzb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect("mongodb+srv://shreerajmane007:1234567890@cluster0.bkgzb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => console.log("Database connected successfully"))
   .catch((err) => console.log(err, "Error connecting to database"));
+
--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 import express from "express"
 import cors from 'cors'
-import dotenv from 'dotenv'
-import connectDB from "./config/mongodb.js"
+import 'dotenv/config'
+import "./config/mongodb.js"
 import connectCloudinary from "./config/cloudinary.js"
 import userRouter from "./routes/userRoute.js"
 import doctorRouter from "./routes/doctorRoute.js"
 import adminRouter from "./routes/adminRoute.js"
 
-dotenv.config();
-
 // app config
 const app = express()
 const port = process.env.PORT || 4000
 app.use(cors())
-connectDB()
 connectCloudinary()
 
 // middlewares
