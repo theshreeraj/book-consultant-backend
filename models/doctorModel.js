e0e8b9fda7ce944a5b1c1bc8c3c6ed534a27ee9a
theshreeraj <shreerajmane007@gmail.com>
2024-12-25 23:30:08 +0530

    [bugfix] :

--- models/doctorModel.js
+++ models/doctorModel.js
@@ -16,5 +16,5 @@ const doctorSchema = new mongoose.Schema({
     date: { type: Number, required: true },
 }, { minimize: false })
 
-const doctorModel = mongoose.models.doctor || mongoose.model("doctor", doctorSchema);
+const doctorModel = mongoose.model("doctor", doctorSchema);
 export default doctorModel;
